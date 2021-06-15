78ed9ade4f24e2bffe1a6175354817024ce09f0e
Anukul Sangwan <anukulsangwan@icloud.com>
2021-06-15 19:53:17 +0530

    update webpack publicPath

--- nwb.config.js
+++ nwb.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   type: "react-app",
   webpack: {
+    publicPath: "",
     define: {
       "process.env.REACT_APP_API_URL": JSON.stringify(
         process.env.REACT_APP_API_URL
